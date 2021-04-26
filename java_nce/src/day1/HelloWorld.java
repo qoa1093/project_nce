eade3d40beebce74979dff839cc1f8f0ba39d41f
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-04-26 17:34:28 +0900

    자바1일차: 헬로월드(주석예제)

---
+++ java_nce/src/day1/HelloWorld.java
@@ -0,0 +1,18 @@
+package day1;
+
+/**
+ * 도큐먼트 주석
+ * 흑흑
+ * */
+public class HelloWorld {
+	//주석
+	/* 작성일 : 2021-04-26
+	 * 작성자 : 노채은
+	 * 내용  : 기본 프로젝트 생성 및 테스트 
+	 **/
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
