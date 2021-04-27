5a4b2f4d399453e176c8afeac94c4f11b782ac90
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-04-27 17:43:16 +0900

    자바2일차:if문예제1_07

---
+++ java_nce/src/day2/if_1.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class if_1 {
+
+	public static void main(String[] args) {
+		// if문 예제_1  07
+		int num = 3;
+		
+		if(num == 0){
+			System.out.println("0입니다.");
+		} else if(num == 1) {
+			System.out.println("1입니다.");
+		} else {
+			System.out.println("0과 1이 아닙니다.");
+		}
+
+	}
+
+}
