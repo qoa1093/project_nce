108d366b92369541a68c1a92f6319df3589ae87a
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-04-27 17:44:13 +0900

    자바2일차:부호연산자예제01

---
+++ java_nce/src/day2/signOperator.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class signOperator {
+
+	public static void main(String[] args) {
+		// 부호 연산자 예제01 : 마이너스 붙이기
+		
+		
+		int num1 = 10;
+		System.out.println("-num1 = " + (-num1));
+		int num2 = -20;
+		System.out.println("-num2 = " + (-num2));
+
+	}
+
+}
