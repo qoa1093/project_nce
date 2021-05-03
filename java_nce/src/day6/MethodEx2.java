c89375bf76a9f7c3a25a03f3690d1918d424867e
qoa1193 <83192174+ppp294@users.noreply.github.com>
2021-05-03 17:38:07 +0900

    자바6일차:07MethodEx02

---
+++ java_nce/src/day6/MethodEx2.java
@@ -0,0 +1,46 @@
+package day6;
+
+public class MethodEx2 {
+
+	public static void main(String[] args) {
+		int arr[] = new int[] {1,2,3,4,5};
+		printArray(arr);
+		initArray(arr,0);
+		printArray(arr);
+		
+	}
+	/*기능 : 정수 배열에 있는 모든 원소값을 출력하는 메소드					//접기 가능
+	 * 매개변수 : 정수 배열 => int arr[]
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : printArray
+	 * 
+	 * */
+	public static void printArray(int arr[]) {
+		/*향상된 for문
+		 *  - 배열이나 컬렉션프레임워크
+		 *  - 전체를 탐색
+		 *  - 수정이 아닌 경우*/
+		
+		for(int tmp:arr){
+			System.out.print(tmp+" ");
+		}
+		System.out.println();
+	}
+	/* 기능 : 배열에 지정된 정수로 모두 초기화하는 메소드 ??
+	 * 매개변수 : 정수 배열, 지정된 정수 => (int[]arr, int num)
+	 * 리턴타입 : 없음 -> void(다시 돌려놓을 필요가 없음. 알려준 배열에 수정을 했기 때문에.) 
+	 * 메소드명 : initArray => 초기화배열?*/
+	
+	public static void initArray(int arr[], int num) {
+		//예외처리, 배열이 생성이 안된경우 메소드를 종료
+		 if(arr == null) {
+			 return;
+		 }
+		 int i;
+		 for(i=0;i<arr.length;i+=1) {
+			 arr[i] = num;
+		 }
+		//[1,2,3,4,5],0(초기화 하려는 정수가 주어져야 함)
+		//[0,0,0,0,0]
+	}
+}
