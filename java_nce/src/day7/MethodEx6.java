b4560ef355256bf29e810593d51e64ca679f8a1b
qoa1193 <83192174+ppp294@users.noreply.github.com>
2021-05-04 17:19:49 +0900

    자바7일차:04MethodEx6

---
+++ java_nce/src/day7/MethodEx6.java
@@ -0,0 +1,93 @@
+package day7;
+
+public class MethodEx6 {
+
+	public static void main(String[] args) {
+		/*다음과 같이 출력하는 메소드를 만드세요.
+		 * *
+		 * **
+		 * ***
+		 * ****
+		 * *****
+		 * */
+		/* 다음과 같이 출력하는 메소드를 만드세요.
+		 *     *
+		 *    **
+		 *   ***
+		 *  ****
+		 * *****
+		 * */
+		printChar1('+',6);
+		printChar2('*',5);
+		printRightTriangle('+',6);
+		printLeftTriangle('+',6);
+		printTriangle('*',5,TriangleShape.RIGHT_TRIANGLE);
+
+	}
+	public static void printChar1(char ch,int num1) {
+		for(int i=1;i<=num1;i+=1) {
+			for(int k=1;k<=i;k+=1) {
+				System.out.print(ch);
+			}
+			System.out.println();	
+		}
+	}
+	public static void printChar2(char ch, int num1) {
+		for(int i=1;i<=num1;i+=1) {
+			for(int j=num1-1;j>=i;j-=1) {
+				System.out.print(" ");
+			}
+			for(int k=1;k<=i;k+=1) {
+				System.out.print(ch);
+			}
+			System.out.println();	
+		}
+	}
+	/* 기능 : 주어진 문자를 이용하여 왼쪽으로 붙은 삼각형을 n줄 출력하는 메소드
+	 * 매개변수 : 문자, n줄 => char ch, int n
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : printLeftTriangle*/
+	public static void printLeftTriangle(char ch, int n) {
+		for(int i=1;i<=n;i+=1) {
+			for(int k =1;k<=i;k+=1) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+	
+	}
+	/* 기능 : 주어진 문자를 이용하여 오른쪽으로 붙은 삼각형을 n줄 출력하는 메소드
+	 * 매개변수 : 문자, n줄 => char ch, int n
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : printRightTriangle*/
+	public static void printRightTriangle(char ch, int n) {
+		for(int i=1;i<=n;i+=1) {
+			for(int k =1;k<=n-i;k+=1) {
+				System.out.print(" ");
+			}
+			for(int k =1;k<=i;k+=1) {
+				System.out.print(ch);
+			}
+			System.out.println();
+		}
+		
+	}
+	/* 기능 : 주어진 문자를 이용하여 n줄 삼각형 출력하는데 주어진 삼각형 종류에 맞게 출력하는 메소드 
+	 * 매개변수 : 문자, n줄, 삼각형 종류 => char ch, int n, TriangleShape
+	 * 리턴타입 : 없음 => void
+	 * 메소드명 : printTriangle */
+
+	public static void printTriangle(char ch, int n, TriangleShape shape) {
+		switch(shape) {
+		case LEFT_TRIANGLE:
+			printLeftTriangle(ch, n);
+			break;
+		case RIGHT_TRIANGLE:
+			printRightTriangle(ch, n);
+			break;
+		}
+	}
+	
+
+}
+enum TriangleShape{LEFT_TRIANGLE,RIGHT_TRIANGLE}
