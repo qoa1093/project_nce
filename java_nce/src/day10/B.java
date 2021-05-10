cd7feca58a2e651dcde425f00167f9b48d8c828a
qoa1193 <83192174+ppp294@users.noreply.github.com>
2021-05-10 17:31:33 +0900

    자바10일차:01class접근제한자

---
+++ java_nce/src/day10/B.java
@@ -0,0 +1,5 @@
+package day10;
+
+public class B {
+	protected int num;
+}
---
+++ java_nce/src/day10/D.java
@@ -0,0 +1,15 @@
+package day10;
+
+public class D {
+	private int num;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+	
+	
+}
