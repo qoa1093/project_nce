c6ac132f306a7214b1cdfd2eae10ac1a433d9d63
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-05-11 17:43:20 +0900

    자바10일차:사각형 리사이즈(수정)

--- java_nce/src/day10/ClassEx6.java
+++ java_nce/src/day10/ClassEx6.java
@@ -11,7 +11,12 @@ public class ClassEx6 {
 		Point rd1 = new Point(20,0);
 		Rect r3 = new Rect(lu1, rd1);
 		r3.print();
-		lu1.setX(-10);			//좌표 점 하나 바꿨는데 사각형 r3의 좌표 점 위치가가 덩달아 바뀌는 문제가 생김 나머지는 내일시간에
+		lu1.setX(-10);			//좌표 점 하나 바꿨는데 사각형 r3의 좌표 점 위치가가 덩달아 바뀌는 문제가 생김 => 복사생성자를 이용하여 해결
+		r3.print();
+		System.out.println();
+		r3.move(-20, 0);
+		r3.print();
+		r3.resize(10, 10);
 		r3.print();
 	}
 
