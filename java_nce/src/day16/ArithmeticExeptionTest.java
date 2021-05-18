842787eb87b1df11bc4e1c585a36b3999b72dc0c
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-05-18 17:22:32 +0900

    자바16일차:01ArithmeticExeption

---
+++ java_nce/src/day16/ArithmeticExeptionTest.java
@@ -0,0 +1,12 @@
+package day16;
+
+public class ArithmeticExeptionTest {
+
+	public static void main(String[] args) {
+		// 정수/정수에서 0으로 나눌 때 발생하는 예외 
+		int num1 = 1, num2 = 0;
+		System.out.println(num1/num2);
+
+	}
+
+}
