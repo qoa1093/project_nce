c2ff2ef7e9e5d8c488c7bac8b3319d6cc6f58801
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-05-20 03:14:33 +0900

    13일차수정중(집)

--- java_nce/src/day13/CarTest.java
+++ java_nce/src/day13/CarTest.java
@@ -3,7 +3,7 @@ package day13;
 public class CarTest {
 
 	public static void main(String[] args) {
-		// 
+		
 		AutoCar ac = new AutoCar("1종", "기아");
 		StickCar sc = new StickCar("2종","현대");
 		ac.turnOn();
@@ -15,15 +15,16 @@ public class CarTest {
 		System.out.println("------------");
 		Car c = ac;
 		c.print();
-		
-		//Car c2 = new Car("1종", "기아"); //에러남
+		System.out.println("--------------");
+		//Car c2 = new Car("1종", "기아"); //에러남 @@@
 		Car c2 = new AutoCar("1종","기아");
 		ac = (AutoCar)c2;
 		ac.print();
 		//차를 관리하는데 수동은 수동끼리 자동은 자동끼리 관리
 		AutoCar [] acList = new AutoCar[5];
 		StickCar [] scList = new StickCar[5];
-		
+		acList[0] = ac;
+		scList[0] = sc;
 		//차를 관리하는데 수동과 자동 상관없이 같이 관리
 		Car [] cList = new Car[5];
 		cList[0] = ac;
