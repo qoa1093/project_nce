847625ed43b65b30766ae928c1947a50012df509
qoa1193 <>
2021-06-25 16:29:15 +0900

    6월25일 예제

--- front/JS 수업/js/rolling.js
+++ front/JS 수업/js/rolling.js
@@ -38,13 +38,35 @@ function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
 
 function rollingLeft(list, listItem, animateTime, intervalTime){
 	var id = setInterval(function(){
-		if($(list +' ' + listItem).first()){
+		if(!$(list +' ' + listItem).first().is(':animated')){
 			var width = $(list +' ' + listItem).first().width();
 			$(list +' ' + listItem).first()
 				.animate({'margin-left':'-'+width}, animateTime,function(){
 				$(this).detach().appendTo(list).removeAttr('style');
 			})
 		}
-	},1500);
+	},intervalTime);
 	return id;
+}
+function rollingRight(list, listItem, animateTime, intervalTime){
+	var id = setInterval(function(){
+		if(!$(list + ' ' + listItem).first().is(':animated')){
+		var width = $(list + ' ' + listItem).last().width();
+		$(list + ' ' + listItem).last().detach().prependTo(list).css('margin-left','-'+width+'px')
+		.animate({'margin-left':'0px'},animateTime);
+		}
+	},intervalTime);
+	return id;
+}
+function rolling(direction, list, listItem, margin, animateTime, intervalTime){
+ 
+ if(direction.toLowerCase() == 'left'){	//방향 .소문자로 바꿔줌
+	return rollingLeft(list, listItem, animateTime, intervalTime);
+ }
+ else if(direction.toLowerCase() == 'right'){
+	 return rollingRight(list, listItem, animateTime, intervalTime);
+ }
+ else{
+	 return rollingTop(list, listItem, margin, animateTime, intervalTime);
+ }
 }
