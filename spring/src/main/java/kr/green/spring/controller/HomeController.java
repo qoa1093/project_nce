f76048a75b903aaf4977816b46dbf0a8fbffd1fd
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-07-20 17:40:55 +0900

    spring : ajax 활용한 로그인 확인 기능

--- spring/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring/src/main/java/kr/green/spring/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -119,6 +120,15 @@ public class HomeController {
 		MemberVO user = memberService.getMember(id);
 		String res = user != null? "IMPOSSIBLE" : "POSSIBLE";
 		return res;
+	}
+	@ResponseBody
+	@PostMapping(value = "/member/signin")//경로에 있는 변수 //가져와서 문자열로 저장
+	public String memberSigninPost(@RequestBody MemberVO user, HttpServletRequest r) {
+		//System.out.println(user);
+		MemberVO dbUser = memberService.signin(user);
+		if(dbUser != null)
+			r.getSession().setAttribute("user", dbUser);
+		return dbUser != null ? "success" : "fail";
 	}
 	
 }
