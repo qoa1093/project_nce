d49e0ed12332cf59998eadb58684ba56dbf50b88
qoa1193 <83192200+qoa1193@users.noreply.github.com>
2021-08-04 00:12:05 +0900

    STUDY : 일반/공지 게시판 댓글 기능 구현(등록,수정,삭제,새로고침,모듈화)

---
+++ study/src/main/java/kr/green/study/service/ReplyServiceImp.java
@@ -0,0 +1,43 @@
+package kr.green.study.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import kr.green.study.dao.BoardDAO;
+import kr.green.study.dao.ReplyDAO;
+import kr.green.study.pagination.Criteria;
+import kr.green.study.vo.BoardVO;
+import kr.green.study.vo.MemberVO;
+import kr.green.study.vo.ReplyVO;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ReplyServiceImp implements ReplyService{
+	private ReplyDAO replyDao;
+	private BoardDAO boardDao;
+
+	@Override
+	public String insertReply(ReplyVO reply, MemberVO user) {
+		if(reply == null || reply.getRp_bd_num() <=0 || user == null)
+			return "FAIL";
+		BoardVO board = boardDao.selectBoard(reply.getRp_bd_num());
+		if(board == null || board.getType().equals("IMAGE"))
+			return "FAIL";
+		reply.setRp_me_id(user.getId());
+		replyDao.insertReply(reply);
+		return "OK";
+	}
+
+	@Override
+	public ArrayList<ReplyVO> getReplyList(int rp_bd_num, Criteria cri) {
+		
+		return replyDao.selectReplyList(rp_bd_num, cri);
+	}
+
+	@Override
+	public int getTotalCount(int rp_bd_num) {
+		return replyDao.selectTotalCount(rp_bd_num);
+	}
+}
